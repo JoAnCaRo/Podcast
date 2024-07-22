db4f0286d3a2750a1c4fd68f90ff8825f05c9967
JauseJones <jausejones@jausejonessmini.localdomain>
2024-07-22 15:37:34 +0200

    Data created

--- podcast_data.py
+++ podcast_data.py
@@ -1,1 +1,36 @@
-cl
+themes = [
+    "Documental", # De Eso No Se Habla / Gabinete de Curiosidades
+    "Noticias", # A Vivir Que Son Dos Días / Saldremos Mejores
+    "Educación", # Aprender de Grandes / Se Habla Código
+    "Política", # Carne Cruda
+    "Humor", # Aquí Hay Dragones / La Ruina / Nadie Sabe Nada
+    "Tecnología", # Código Futuro
+    "Astronomía", # Coffe Break
+    "Cultura", # En La Redacción / Suena Carmena
+    "Filosofía", # Filosofía Aplicada
+    "Historia", # La Escóbula De La Brújula
+    "Naturaleza", # La Senda De Las Plantas Perdidas
+    "Superación Personal", # Mientras Respires Estás a Tiempo
+    "TV & Film" # Todopoderosos
+]
+
+data = [["De Eso No Se Habla", "8 Temporadas", "5", "Documental", "En este podcast hablamos de los silencios que crea esa frase, tanto en nosotras como en la sociedad: de las historias que se esconden detrás de ellos, y de qué pasa cuando los rompemos."],
+        ["Gabinete de Curiosidades", "4 Temporadas", "5", "Documental", "Historias que atraviesan continentes y llegan al espacio, con protagonistas tan distintos como cortesanas victorianas, jefes indios o secretarios de la Casa Blanca. Acompáñanos. Porque si perdemos la curiosidad, ¿Qué nos queda?"],
+        ["A Vivir Que Son Dos Días", "20 Temporadas", "4", "Noticias", "Para aprender a mirar de otra forma, tu tienda de campaña que aísla las mañanas del fin de semana de la frenética actualidad diaria."],
+        ["Saldremos Mejores", "7 Temporadas", "4", "Noticias", "Con rigor y desparpajo, Inés Hernand y Nerea Pérez de las Heras te llevan a profundizar temas cruciales junto a voces expertas, cada miércoles."],
+        ["Aprender de Grandes", "10 Temporadas", "3", "Educación", "El podcast donde Gerry Garbulsky comparte lo que aprende conversando con gente que admira."],
+        ["Se Habla Código", "3 Temporadas", "3", "Educación", "Bienvenidos a Se Habla Código, un podcast sobre programación para web y anécdotas del programador."],
+        ["Carne Cruda", "15 Temporadas", "5", "Política", "Podcast de agitación social, contracultura, crítica política, humor y música dirigido por Javier Gallego Crudo."],
+        ["Aquí Hay Dragones", "7 Temporadas", "5", "Humor", "Un podcast desopilante en el que cuatro humoristas y presentadores conversan en torno a temas tan diversos como interesantes en clave de comedia."],
+        ["La Ruina", "6 Temporadas", "5", "Humor", "Tomàs Fuentes e Ignasi Taltavull comentan y juzgan la peor anécdota de la gente que viene de público al programa."],
+        ["Nadie Sabe Nada", "11 Temporadas", "4", "Humor", "Andreu Buenafuente y Berto Romero se sientan frente a frente, micro a micro, e improvisan. ¿Qué puede salir mal?"],
+        ["Código Futuro", "2 Temporadas", "3", "Tecnología", "Un podcast para acercarnos al mundo digital y a las tecnologías emergentes que traen consigo cambios sociales."],
+        ["Coffe Break", "9 Temporadas", "4", "Astronomía", "La tertulia semanal en la que repasamos las últimas noticias de la actualidad científica."],
+        ["En La Redacción", "1 Temporada", "4", "Cultura", "Un espacio de encuentro informal, exclusivo para socios y socias de elDiario.es, con invitados de diferentes ámbitos culturales, sociales o políticos."],
+        ["Suena Carmena", "5 Temporadas", "3", "Cultura", "Un podcast para escuchar conversaciones tranquilas sobre temas que nos darán qué pensar y sobre todo, destruir tópicos, porque los tópicos nos impiden avanzar."],
+        ["Filosofía Aplicada", "5 Temporadas", "4", "Filosofía", "Podcast en el que aprenderemos a utilizar a la filosofía como una herramienta para mejorar nuestra vida."],
+        ["La Escóbula De La Brújula", "12 Temporadas", "5", "Historia", "Una reunión semanal de amigos con curiosidad sobre Historia, cultura y leyendas. "],
+        ["La Senda De Las Plantas Perdidas", "7 Temporadas", "5", "Naturaleza", "Un podcast etnobotánico sobre nuestras historias de amor (y desamor) con el reino vegetal, contadas de viva voz por Aina S. Erice."],
+        ["Mientras Respires Estás a Tiempo", "5 Temporadas", "4", "Charlemos un ratito!! Este será nuestro espacio íntimo y sagrado donde nuestros cerebros y corazones se conecten."],
+        ["Todopoderosos", "8 Temporadas", "5", "TV & Film", "Cine, libros, series, cómics. Con Arturo González-Campos, Rodrigo Cortés, Javier Cansado y Juan Gómez-Jurado. Un programa para unirlos a todos."]   
+]
